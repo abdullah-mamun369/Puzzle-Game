1347d195a84bd2ef7f05056587a1c056e00d75ff
abdullah-mamun369 <maamamun369@gmail.com>
2023-04-12 18:50:04 +0600

    Images are dynamically set to the html

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const img1 = document.getElementById("img-1")
-const img2 = document.getElementById("img-2")
-const img3 = document.getElementById("img-3")
-const img4 = document.getElementById("img-4")
-const img5 = document.getElementById("img-5")
-const img6 = document.getElementById("img-6")
-const img7 = document.getElementById("img-7")
-const img8 = document.getElementById("img-8")
-const img9 = document.getElementById("img-9")
-const img10 = document.getElementById("img-10")
-const img11 = document.getElementById("img-11")
-const img12 = document.getElementById("img-12")
+
+const rows = 3;
+const cols = 4;
+
+// let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5", "12"]
+
+window.onload = () => {
+    for (let i = 0; i < imgOrder.length; i++) {
+
+        // img insert
+        let img = document.createElement("img");
+        img.id = "img" + "-" + imgOrder[i].toString();
+        img.src = "assets/" + imgOrder[i].toString() + ".png"
+
+        // Drag
+
+        document.getElementById("imgBoard").append(img)
+    }
+}
