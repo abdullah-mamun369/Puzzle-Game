b1c66dbb5cdb9df5848887860030472f6c6b8d9c
abdullah-mamun369 <maamamun369@gmail.com>
2023-04-13 16:57:26 +0600

    Puzzle's images ID and SRC values are changes dynamically

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
-const rows = 3;
-const cols = 4;
 
 // let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5", "12"]
+let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5"]
+
+for (let i = 0; i < imgOrder.length; i++) {
+
+    // img insert
+    let img = document.createElement("img");
+    img.setAttribute("class", "border")
+    img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+    img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+
+    document.getElementById("imgBoard").append(img)
+    // Drag
+}
+
+
 
 window.onload = () => {
-    for (let i = 0; i < imgOrder.length; i++) {
+    // for (let i = 0; i < imgOrder.length; i++) {
 
-        // img insert
-        let img = document.createElement("img");
-        img.id = "img" + "-" + imgOrder[i].toString();
-        img.src = "assets/" + imgOrder[i].toString() + ".png"
+    //     // img insert
+    //     let img = document.createElement("img");
+    //     // img.id = "img" + "-" + imgOrder[i].toString();
+    //     // img.src = "assets/" + imgOrder[i].toString() + ".png"
+    //     img.setAttribute("class", "border")
+    //     img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+    //     img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
 
-        // Drag
-        img.addEventListener("dragStart", dragStart)
-        img.addEventListener("dragOver", dragOver)
-        img.addEventListener("dragEnter", dragEnter)
-        img.addEventListener("dragLeave", dragLeave)
-        img.addEventListener("dropDrop", dropDrop)
-        img.addEventListener("dragEnd", dragEnd)
+    //     document.getElementById("imgBoard").append(img)
+    //     // Drag
+    //     document.getElementById("reset").addEventListener("click", () => {
 
-        document.getElementById("imgBoard").append(img)
-    }
+    //     })
+
+    // }
 }
