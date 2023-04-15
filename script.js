8b12ce7b05e5878bf739d52137f6ce96d3492dc9
abdullah-mamun369 <maamamun369@gmail.com>
2023-04-15 16:33:46 +0600

    Puzzle reset function is done

--- script.js
+++ script.js
@@ -3,36 +3,114 @@
 // let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5"]
 
-for (let i = 0; i < imgOrder.length; i++) {
 
-    // img insert
-    let img = document.createElement("img");
-    img.setAttribute("class", "border")
-    img.setAttribute("id", `img-${imgOrder[i].toString()}`)
-    img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+const resetBtn = document.getElementById("reset");
 
-    document.getElementById("imgBoard").append(img)
-    // Drag
+
+function shuffleImg(img) {
+    for (let i = 0; i < img.length; i++) {
+        const j = Math.floor(Math.random() * img.length);
+        [img[i], img[j]] = [img[j], img[i]];
+    }
 }
-
-
 
 window.onload = () => {
-    // for (let i = 0; i < imgOrder.length; i++) {
 
-    //     // img insert
-    //     let img = document.createElement("img");
-    //     // img.id = "img" + "-" + imgOrder[i].toString();
-    //     // img.src = "assets/" + imgOrder[i].toString() + ".png"
-    //     img.setAttribute("class", "border")
-    //     img.setAttribute("id", `img-${imgOrder[i].toString()}`)
-    //     img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+    let div = document.createElement("div");
+    div.setAttribute("id", "imgContainer")
+    div.setAttribute("class", "grid grid-cols-3 ")
+    for (let i = 0; i < imgOrder.length; i++) {
+        // img insert
+        let img = document.createElement("img");
+        img.setAttribute("class", "border")
+        img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+        img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
 
-    //     document.getElementById("imgBoard").append(img)
-    //     // Drag
-    //     document.getElementById("reset").addEventListener("click", () => {
+        div.append(img)
 
-    //     })
+    }
+    document.getElementById("imgBoard").append(div)
 
-    // }
 }
+
+resetBtn.addEventListener("click", () => {
+    shuffleImg(imgOrder);
+
+    let div = document.createElement("div");
+    div.setAttribute("id", "imgContainer")
+    div.setAttribute("class", "grid grid-cols-3 ")
+
+    for (let i = 0; i < imgOrder.length; i++) {
+
+
+        // img insert
+        let img = document.createElement("img");
+        img.setAttribute("class", "border")
+        img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+        img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+
+        div.append(img)
+
+    }
+    document.getElementById("imgBoard").append(div)
+
+    document.getElementById("reset").setAttribute("disabled", "disabled")
+
+    console.log(imgOrder);
+})
+
+document.getElementById("clear").addEventListener("click", () => {
+    const imageContaimer = document.getElementById("imgContainer")
+    resetBtn.removeAttribute("disabled")
+    imageContaimer.remove();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
