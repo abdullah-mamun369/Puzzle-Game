6e9f26bc13bcc1d57ef79c3ebca07134c7c13a44
abdullah-mamun369 <maamamun369@gmail.com>
2023-04-18 17:32:50 +0600

    reset button is done

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const up = document.getElementById("up")
 const down = document.getElementById("down")
 const right = document.getElementById("right")
 const left = document.getElementById("left")
-const start = document.getElementById("start")
 const keyboard = document.getElementById("keyboard")
+const imageBoard = document.getElementById("imgBoard");
 
-// let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5", "0"]
+// let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let imgOrder = ["3", "9", "1", "7", "6", "4", "8", "5", "2"]
 
 
 const resetBtn = document.getElementById("reset");
@@ -34,11 +34,14 @@ window.onload = () => {
         div.append(img)
 
     }
-    document.getElementById("imgBoard").append(div)
+    imageBoard.append(div)
 
 }
 
 resetBtn.addEventListener("click", () => {
+
+    imageBoard.innerHTML = "";
+
     shuffleImg(imgOrder);
 
     let div = document.createElement("div");
@@ -57,40 +60,199 @@ resetBtn.addEventListener("click", () => {
         div.append(img)
 
     }
-    document.getElementById("imgBoard").append(div)
-
-    document.getElementById("reset").setAttribute("disabled", "disabled")
+    imageBoard.append(div)
 
     console.log(imgOrder);
 
     const image = document.getElementById("img-1")
     console.log(image);
-
-    start.removeAttribute("class", "hidden")
-    start.setAttribute("class", "text-xl text-white hover:bg-red-600 px-5 py-1 border hover:border-red-500 rounded-lg transition duration-300 ease-in-out mb[-20px]")
-
-})
-
-document.getElementById("clear").addEventListener("click", () => {
-    const imageContaimer = document.getElementById("imgContainer")
-    resetBtn.removeAttribute("disabled")
-    imageContaimer.remove();
-
-    start.setAttribute("class", "hidden")
-    keyboard.setAttribute("class", "hidden")
-
 })
 
 
 
-start.addEventListener("click", () => {
-    start.setAttribute("class", "hidden")
-    keyboard.removeAttribute("class", "hidden")
 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const up = document.getElementById("up")
+// const down = document.getElementById("down")
+// const right = document.getElementById("right")
+// const left = document.getElementById("left")
+// const start = document.getElementById("start")
+// const keyboard = document.getElementById("keyboard")
+
+// // let imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+// let imgOrder = ["3", "9", "1", "7", "11", "6", "4", "8", "2", "10", "5", "0"]
+
+
+// const resetBtn = document.getElementById("reset");
+
+
+// function shuffleImg(img) {
+//     for (let i = 0; i < img.length; i++) {
+//         const j = Math.floor(Math.random() * img.length);
+//         [img[i], img[j]] = [img[j], img[i]];
+//     }
+// }
+
+// window.onload = () => {
+
+//     let div = document.createElement("div");
+//     div.setAttribute("id", "imgContainer")
+//     div.setAttribute("class", "grid grid-cols-3 ")
+//     for (let i = 0; i < imgOrder.length; i++) {
+//         // img insert
+//         let img = document.createElement("img");
+//         img.setAttribute("class", "border")
+//         img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+//         img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+
+//         div.append(img)
+
+//     }
+//     document.getElementById("imgBoard").append(div)
+
+// }
+
+// resetBtn.addEventListener("click", () => {
+//     shuffleImg(imgOrder);
+
+//     let div = document.createElement("div");
+//     div.setAttribute("id", "imgContainer")
+//     div.setAttribute("class", "grid grid-cols-3 ")
+
+//     for (let i = 0; i < imgOrder.length; i++) {
+
+
+//         // img insert
+//         let img = document.createElement("img");
+//         img.setAttribute("class", "border")
+//         img.setAttribute("id", `img-${imgOrder[i].toString()}`)
+//         img.setAttribute("src", `assets/${imgOrder[i].toString()}.png`)
+
+//         div.append(img)
+
+//     }
+//     document.getElementById("imgBoard").append(div)
+
+//     document.getElementById("reset").setAttribute("disabled", "disabled")
+
+//     console.log(imgOrder);
+
+//     const image = document.getElementById("img-1")
+//     console.log(image);
+
+//     start.removeAttribute("class", "hidden")
+//     start.setAttribute("class", "text-xl text-white hover:bg-red-600 px-5 py-1 border hover:border-red-500 rounded-lg transition duration-300 ease-in-out mb[-20px]")
+
+// })
+
+// document.getElementById("clear").addEventListener("click", () => {
+//     const imageContaimer = document.getElementById("imgContainer")
+//     resetBtn.removeAttribute("disabled")
+//     imageContaimer.remove();
+
+//     start.setAttribute("class", "hidden")
+//     keyboard.setAttribute("class", "hidden")
+
+// })
+
+
+
+// start.addEventListener("click", () => {
+//     start.setAttribute("class", "hidden")
+//     keyboard.removeAttribute("class", "hidden")
+
+
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
